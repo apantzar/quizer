83932b74a11aadf556debd4881c5ab3f057e0262
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-08-25 20:03:19 +0300

    Just Flutter

---
+++ android/app/src/main/kotlin/com/example/quizernew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizernew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answered');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var question = [
+      'What is your favorite color?',
+      'What is your favorite fruit?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quizer'),
+        ),
+        body: Column(children: <Widget>[
+          Text('Question'),
+          // ignore: deprecated_member_use
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('Answer 2'),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print('Answer 3'),
+          ),
+          RaisedButton(
+              child: Text('Answer 4'), onPressed: () => print('Answer 4')),
+        ]),
+      ),
+    );
+  }
+}
