41ea3014e6ac580a62586303094f675c0ea74848
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-08-26 12:35:16 +0300

    setState()

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,27 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
   void answerQuestion() {
-    questionIndex++;
+    setState(() {
+     
+         questionIndex++;
+      
+     
+    });
+    
 
     //testing
-   // print(questionIndex);
+    // print(questionIndex);
   }
 
   @override
